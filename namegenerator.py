b3c35851e2318d79b914f6e006cd89c5835847e3
jberzz <jburs@proton.me>
2024-01-17 14:21:56 -0600

    Added main

--- namegenerator.py
+++ namegenerator.py
@@ -1,20 +1,33 @@
-import sys, random
+"""
+Title: Random Name Generator
+Author: Jon Burstein
+Details: A program to make funny names.
+"""
+import sys 
+import random
 
-print("Welcome to the dumb name generator.")
-print("These names are dumb as hell.\n")
+def main():
 
-first = ('weiner', 'balls', 'gorgina', 'holden', 'hubert', 'dingus', 'chuck', 'remus', 'ribbert')
-last = ('nutsack','Mcdoodie', 'scrotus','bingus', 'mcdoogle', '9/11 was an inside job', 'dickhole')
+    print("Welcome to the dumb name generator.")
+    print("These names are dumb as hell.\n")
 
-while True:
-    firstName = random.choice(first)
-    lastName = random.choice(last)
+    first = ('weiner', 'balls', 'gorgina', 'holden', 
+             'hubert', 'dingus', 'chuck', 'remus', 'ribbert')
+    last = ('nutsack','Mcdoodie', 'scrotus','bingus', 
+            'mcdoogle', '9/11 was an inside job', 'dickhole')
 
-    print("{} {}".format(firstName, lastName, file=sys.stderr))
-    print("\n")
+    while True:
+        first_name = random.choice(first)
+        last_name = random.choice(last)
 
-    try_again = input("\n\nTry again? Press enter else n to quit\n")
-    if try_again.lower() == "n":
-        break
+        print("{} {}".format(first_name, last_name, file=sys.stderr))
+        print("\n")
 
-    input("\npress Enter to exit.")
+        try_again = input("\n\nTry again? Press enter else n to quit\n")
+        if try_again.lower() == "n":
+            break
+
+        input("press Enter to exit.\n")
+"""the __name__ is a built in variable that distinguishes stand alone or an imported module"""
+if __name__ == "__main__":
+    main()

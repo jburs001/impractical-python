305a32d60ecb17c7859ce0a110a31e051945fd3c
jberzz <jburs@proton.me>
2024-01-18 13:40:28 -0600

    updating style

--- piglatin.py
+++ piglatin.py
@@ -1,5 +1,6 @@
 """Progrom to convert words to pig latin"""
 
+"""Function to determine if a word bgins with a vowel"""
 def vowel(word):
     vowels = ('a', 'e', 'i', 'o','u')
     is_vowel = False
@@ -7,17 +8,18 @@ def vowel(word):
         is_vowel = True
     return is_vowel
 
+"""Function to convert word to piglatin"""
 def piglatin(word):
     is_vowel = vowel(word)
     vowel_end = "way"
     cons_end = "ay"
-    if is_vowel == True:
+    if is_vowel is True:
         word = word + vowel_end
         return word
-    else:
-        mod_word = word[1:] + word[0]
-        mod_word = mod_word + cons_end
-        return mod_word
+        
+    mod_word = word[1:] + word[0]
+    mod_word = mod_word + cons_end
+    return mod_word
 
 word = input("Please enter a word.\n")
 print(f"{word} in pig latin is {piglatin(word)}")

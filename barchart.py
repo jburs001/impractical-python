e22bbdf36b8adf445e8e5c783917137d10d671d3
jberzz <jburs@proton.me>
2024-01-18 15:47:21 -0600

    Started barchart

---
+++ barchart.py
@@ -0,0 +1,11 @@
+"""
+Program to take a sentence and output how many times
+a letter appears in a bar chart.
+Author: Jon Burstein
+"""
+import pprint
+import collections
+import defaultdict
+
+sentence = input("Please enter a sentence.")
+

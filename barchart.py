537e95d7b7e22682fa7aea807a0b6027273719e5
jberzz <jburs@proton.me>
2024-01-20 20:32:01 -0600

    done

--- barchart.py
+++ barchart.py
@@ -3,9 +3,18 @@ Program to take a sentence and output how many times
 a letter appears in a bar chart.
 Author: Jon Burstein
 """
-import pprint
-import collections
-import defaultdict
+from pprint import pprint
+from collections import defaultdict
 
-sentence = input("Please enter a sentence.")
+sentence = input("Please enter a sentence\n")
 
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+#default dict builds dictionary keys
+barchart = defaultdict(list)
+for letter in sentence:
+    letter = letter.lower()
+    if letter in ALPHABET:
+        barchart[letter].append(letter)
+
+pprint(barchart, width = 110)
